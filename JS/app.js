314e67bd7570b95e3f387b74cda9618ed351e6e7
Brandon <brandongault8@gmail.com>
2021-11-03 16:38:28 -0400

    alert banner 'x' is on the right now

--- JS/app.js
+++ JS/app.js
@@ -8,8 +8,8 @@ alertBanner.innerHTML =
 `
 <div class="alert-banner">
   <p><strong>Alert:</strong> You have unread messages</p>
-  <p class="alert-banner-close">x</p>
   </div>
+  <p class="alert-banner-close">x</p>
 `
 
 alertBanner.addEventListener('click', e => {
